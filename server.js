a013d9305c36b306c626b281888c731d83e800b6
julio@upplify.com <jfujimaki@zenbanx.com>
2016-01-24 09:10:12 -0500

    Initial repo commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log("this route is private");
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + (new Date().toString()) + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+//application level middleware
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('<h1>this is my about page</h1>');
+});
+
+app.use(express.static(__dirname + "/public"));
+app.listen(PORT, function(){
+	console.log('server running on ' + PORT);
+});
